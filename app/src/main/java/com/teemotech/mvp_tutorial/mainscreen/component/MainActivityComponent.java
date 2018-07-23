45c265216a67bd5a41f42b2ec4131db86679a74b
Amine Elouattar <elouattar@gmail.com>
2018-07-23 09:25:08 +0100

    init dagger app component in the application entry.

--- app/src/main/java/com/teemotech/mvp_tutorial/mainscreen/component/MainActivityComponent.java
+++ app/src/main/java/com/teemotech/mvp_tutorial/mainscreen/component/MainActivityComponent.java
@@ -1,6 +1,7 @@
 package com.teemotech.mvp_tutorial.mainscreen.component;
 
 import com.teemotech.mvp_tutorial.data.component.AppComponent;
+import com.teemotech.mvp_tutorial.mainscreen.MainActivity;
 import com.teemotech.mvp_tutorial.mainscreen.module.MainActivityModule;
 import com.teemotech.mvp_tutorial.utils.ActivityScope;
 
@@ -9,4 +10,5 @@ import dagger.Component;
 @ActivityScope
 @Component(dependencies = AppComponent.class, modules = MainActivityModule.class)
 public interface MainActivityComponent {
+    void inject(MainActivity mainActivity);
 }
